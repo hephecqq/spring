60558aef14e30656cd6e06f9bfd9cf558d25d630
Administrator <Administrator@QH-20150815QEPL>
2015-11-02 17:53:02 +0800

    commit

---
+++ spring/src/com/hephec/spring/Car.java
@@ -0,0 +1,54 @@
+package com.hephec.spring;
+
+public class Car {
+
+	private String brand;
+	private String corp;
+	private double price;
+	
+	public Car(String brand, String corp, int maxSpeed) {
+		super();
+		this.brand = brand;
+		this.corp = corp;
+		this.maxSpeed = maxSpeed;
+	}
+	public Car() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Car [brand=" + brand + ", corp=" + corp + ", price=" + price
+				+ ", maxSpeed=" + maxSpeed + "]";
+	}
+	public Car(String brand, String corp, double price) {
+		super();
+		this.brand = brand;
+		this.corp = corp;
+		this.price = price;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getCorp() {
+		return corp;
+	}
+	public void setCorp(String corp) {
+		this.corp = corp;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+	private int maxSpeed;
+}
--- spring/src/com/hephec/spring/Main.java
+++ spring/src/com/hephec/spring/Main.java
@@ -17,10 +17,21 @@ public class Main {
 		ApplicationContext ctx=new ClassPathXmlApplicationContext("application.xml");
 		
 		//2.从IOC容器中获取Bean实例
-		HelloWorld helloWorld=(HelloWorld) ctx.getBean("HelloWorld");
+		//HelloWorld helloWorld=(HelloWorld) ctx.getBean("HelloWorld");
 		
 		//3.调用hello方法
-		helloWorld.hello();
+		//helloWorld.hello();
 		
+		//利用类型IOC容器中的Bean，但要求IOC容器中必须置于一个该类型的Bean
+//		HelloWorld helloWorld1=ctx.getBean(HelloWorld.class);
+//		helloWorld1.hello();
+		//依赖注入方式
+		//属性注入
+		//构造器注入
+//		Car car=ctx.getBean(Car.class);
+//		System.out.println(car);
+		
+		Car car2=(Car) ctx.getBean("car2");
+		System.out.println(car2);
 	}
 }
